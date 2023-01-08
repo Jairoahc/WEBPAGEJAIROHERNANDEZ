a3739c8d1b3ca5f34655c473bb48c5a85ff7a819
jairoahc <jairohernandez010399@gmail.com>
2023-01-08 15:14:41 -0500

    Get in tough and modal for stack list added

--- javascript.js
+++ javascript.js
@@ -7,3 +7,11 @@ if (toastTrigger) {
     toast.show()
   })
 }
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
