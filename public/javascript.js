9266a8a2141eb10584f1ae359c2f84c33469e232
jairoahc <jairohernandez010399@gmail.com>
2023-01-08 16:41:12 -0500

    Centering the stack list modal

---
+++ public/javascript.js
@@ -0,0 +1,17 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
